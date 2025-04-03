54030081d858cb61a4bfceda95f2f78332df6543
DatDangg <datdang20112003@gmail.com>
2025-04-03 15:29:30 +0700

    update typeWriter with arr + re-background app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,31 +1,42 @@
 import { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import styles from './Home.module.css'
-import bannerImg from '../assets/image/banner-img.png'
+import styles from './Home.module.css';
+import bannerImg from '../assets/image/banner-img.png';
 
-function TypeWriter({text}) {
-    const [char, setChar] = useState([])
-    useEffect(()=>{
+function TypeWriter({ texts }) {
+    const [char, setChar] = useState([]);
+    let count = 0
+
+
+    useEffect(() => {
         function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve,ms))
+            return new Promise((resolve) => setTimeout(resolve, ms));
         }
 
-        async function type(){
-            for (let i = 0; i < text.length; i++) {
-                setChar((prev) => [...prev, text[i]]);
-                await sleep(200);
-              }
-        
-              await sleep(1000);
-        
-              for (let i = text.length - 1; i >= 0; i--) {
-                setChar((prev) => prev.slice(0, i));
-                await sleep(200);
-              }
+        async function typeLoop() {
+            while (true) {
+                const currentText = texts[count];
+                
+                for (let i = 0; i < currentText.length; i++) {
+                    setChar((prev) => [...prev, currentText[i]]);
+                    await sleep(150);
+                }
+
+                await sleep(1000);
+
+                for (let i = currentText.length; i >= 0; i--) {
+                    setChar((prev) => prev.slice(0, i));
+                    await sleep(100);
+                }
+
+                count ++
+                if (count == texts.length) count = 0 
+                await sleep(500);
+            }
         }
-        type()
-    },[])
-    console.log(text)
+        typeLoop();
+    }, []);
+
     return (
         <span>
             {char}
@@ -40,25 +51,23 @@ function Home() {
             <div className={styles.banner}>
                 <div className='container'>
                     <div className='row'>
-                        <div className='col-lg-7 '>
+                        <div className='col-lg-7'>
                             <div className={styles.bannerInfor}>
                                 <div className={styles.heading}>Hi There!</div>
                                 <div className={styles.headingName}>I'm <span>Dang Dat</span></div>
                                 <div className={styles.typeWriter}>
-                                    <TypeWriter text='Front End Developer'/>
-                                    {/* Front End Developer */}
+                                    <TypeWriter texts={['Front End Developer']} />
                                 </div>
-                                
                             </div>
-                        </div>      
+                        </div>
                         <div className='col-lg-5'>
                             <img className={styles.bannerImg} src={bannerImg} alt='Banner Image'></img>
-                        </div>      
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
