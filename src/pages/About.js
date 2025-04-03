f1e8e211fe2633fa18185db5edc27d780be1b44d
DatDangg <datdang20112003@gmail.com>
2025-04-04 01:09:03 +0700

    Restructuring Home, create about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,190 @@
-import './About.module.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./About.module.css";
 
-function About(){
+function About() {
     return (
-        // <div>About page</div>
-        <></>
-    )
+        <div className={styles.about}>
+            <div className="container">
+                <div className={`row ${styles.aboutInfor}`}>
+                    <div className="col-lg-12">
+                        <div className={styles.aboutTitle}>
+                            Know Who <span>I'M</span>
+                        </div>
+                        <p>
+                            Eiusmod ipsum sunt Lorem nulla fugiat Lorem anim nulla. In mollit
+                            adipisicing dolore ut culpa elit consectetur eiusmod irure nostrud
+                            Lorem. Eu aliqua anim irure cupidatat incididunt magna commodo
+                            adipisicing officia nisi ad. Reprehenderit dolor eiusmod nulla in
+                            duis ex culpa consequat et nostrud cupidatat enim non ex. Eu
+                            veniam et eiusmod nostrud irure eiusmod veniam amet reprehenderit
+                            ex aliqua. Fugiat reprehenderit ex qui occaecat incididunt
+                            pariatur adipisicing id dolor magna eu. Ex enim reprehenderit ut
+                            officia cupidatat sit exercitation consequat nostrud cillum est
+                            laborum ut.
+                        </p>
+                        <p>
+                            Eiusmod ipsum sunt Lorem nulla fugiat Lorem anim nulla. In mollit
+                            adipisicing dolore ut culpa elit consectetur eiusmod irure nostrud
+                            Lorem. Eu aliqua anim irure cupidatat incididunt magna commodo
+                            adipisicing officia nisi ad. Reprehenderit dolor eiusmod nulla in
+                            duis ex culpa consequat et nostrud cupidatat enim non ex. Eu
+                            veniam et eiusmod nostrud irure eiusmod veniam amet reprehenderit
+                            ex aliqua. Fugiat reprehenderit ex qui occaecat incididunt
+                            pariatur adipisicing id dolor magna eu. Ex enim reprehenderit ut
+                            officia cupidatat sit exercitation consequat nostrud cillum est
+                            laborum ut.
+                        </p>
+                        <p>
+                            Eiusmod ipsum sunt Lorem nulla fugiat Lorem anim nulla. In mollit
+                            adipisicing dolore ut culpa elit consectetur eiusmod irure nostrud
+                            Lorem. Eu aliqua anim irure cupidatat incididunt magna commodo
+                            adipisicing officia nisi ad. Reprehenderit dolor eiusmod nulla in
+                            duis ex culpa consequat et nostrud cupidatat enim non ex. Eu
+                            veniam et eiusmod nostrud irure eiusmod veniam amet reprehenderit
+                            ex aliqua. Fugiat reprehenderit ex qui occaecat incididunt
+                            pariatur adipisicing id dolor magna eu. Ex enim reprehenderit ut
+                            officia cupidatat sit exercitation consequat nostrud cillum est
+                            laborum ut.
+                        </p>
+                        <div className={styles.quote}>"Love pain to be the best"</div>
+                    </div>
+                </div>
+
+                <div className={`row ${styles.aboutSkill}`}>
+                    <div className={styles.aboutHeading}>
+                        Profesional <span>Skillset</span>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M12.207 16.278C11.1241 17.343 9.63879 18 8 18C4.68629 18 2 15.3137 2 12C2 8.68629 4.68629 6 8 6C9.67492 6 11.1896 6.6863 12.278 7.79303L13.6923 6.37878C12.2418 4.91014 10.2272 4 8 4C3.58172 4 0 7.58172 0 12C0 16.4183 3.58172 20 8 20C10.1911 20 12.1764 19.1192 13.6212 17.6923L12.207 16.278Z"
+                                fill="currentColor"
+                            ></path>
+                            <path
+                                d="M15 9H13V11H11V13H13V15H15V13H17V11H15V9Z"
+                                fill="currentColor"
+                            ></path>
+                            <path
+                                d="M20 9H22V11H24V13H22V15H20V13H18V11H20V9Z"
+                                fill="currentColor"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            version="1.1"
+                            viewBox="0 0 32 32"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M4.665 3.411l2.063 23.176 9.258 2.574 9.284-2.578 2.065-23.172h-22.671zM8.951 8.911l-0.068-0.763h7.107v2.842h-4.005l0.259 2.911h3.746v2.842h-6.341l-0.698-7.833zM22.518 14.665l-0.667 7.483-0.043 0.48-5.822 1.616-5.814-1.616-0.398-4.463h2.849l0.202 2.267 3.163 0.854 3.165-0.856 0.329-3.686h-3.485v-2.842h6.587l-0.069 0.763zM23.032 8.911l-0.129 1.441-0.057 0.639h-6.846v-2.842h7.1l-0.068 0.762z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            version="1.1"
+                            viewBox="0 0 32 32"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M16.017 21.044v0zM4.743 3.519l2.049 22.981 9.194 2.552 9.22-2.556 2.051-22.977h-22.514zM23 8.775l-0.693 7.767h-0l-0.48 5.359-0.042 0.476-5.781 1.603-5.773-1.603-0.395-4.426h2.829l0.201 2.248 3.142 0.847 0.008-0.002 0.002-0 3.134-0.846 0.329-3.655-6.579 0-0.056-0.633-0.129-1.429-0.067-0.756 7.081-0 0.258-2.886h-10.786l-0.056-0.634-0.129-1.429-0.067-0.756h14.118l-0.068 0.756z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            role="img"
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title></title>
+                            <path
+                                fill="currentColor"
+                                d="M11.77 11.24H9.956V8.202h2.152c1.17 0 1.834.522 1.834 1.466 0 1.008-.773 1.572-2.174 1.572zm.324 1.206H9.957v3.348h2.231c1.459 0 2.232-.585 2.232-1.685s-.795-1.663-2.326-1.663zM24 11.39v1.218c-1.128.108-1.817.944-2.226 2.268-.407 1.319-.463 2.937-.42 4.186.045 1.3-.968 2.5-2.337 2.5H4.985c-1.37 0-2.383-1.2-2.337-2.5.043-1.249-.013-2.867-.42-4.186-.41-1.324-1.1-2.16-2.228-2.268V11.39c1.128-.108 1.819-.944 2.227-2.268.408-1.319.464-2.937.42-4.186-.045-1.3.968-2.5 2.338-2.5h14.032c1.37 0 2.382 1.2 2.337 2.5-.043 1.249.013 2.867.42 4.186.409 1.324 1.098 2.16 2.226 2.268zm-7.927 2.817c0-1.354-.953-2.333-2.368-2.488v-.057c1.04-.169 1.856-1.135 1.856-2.213 0-1.537-1.213-2.538-3.062-2.538h-4.16v10.172h4.181c2.218 0 3.553-1.086 3.553-2.876z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            version="1.1"
+                            viewBox="0 0 32 32"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M9.633 7.968h3.751v10.514c0 4.738-2.271 6.392-5.899 6.392-0.888 0-2.024-0.148-2.764-0.395l0.42-3.036c0.518 0.173 1.185 0.296 1.925 0.296 1.58 0 2.567-0.716 2.567-3.282v-10.489zM16.641 20.753c0.987 0.518 2.567 1.037 4.171 1.037 1.728 0 2.641-0.716 2.641-1.826 0-1.012-0.79-1.629-2.789-2.32-2.764-0.987-4.59-2.517-4.59-4.961 0-2.838 2.394-4.985 6.293-4.985 1.9 0 3.258 0.37 4.245 0.839l-0.839 3.011c-0.642-0.321-1.851-0.79-3.455-0.79-1.629 0-2.419 0.765-2.419 1.604 0 1.061 0.913 1.53 3.085 2.369 2.937 1.086 4.294 2.616 4.294 4.985 0 2.789-2.122 5.158-6.688 5.158-1.9 0-3.776-0.518-4.714-1.037l0.765-3.085z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            version="1.1"
+                            viewBox="0 0 32 32"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                fill="currentColor"
+                                d="M26.852 15.281l-9.848-9.848c-0.567-0.567-1.487-0.567-2.054 0l-2.045 2.045 2.594 2.594c0.603-0.204 1.294-0.067 1.775 0.413 0.483 0.483 0.619 1.181 0.41 1.786l2.5 2.5c0.605-0.209 1.303-0.074 1.786 0.41 0.675 0.675 0.675 1.769 0 2.444s-1.769 0.675-2.445 0c-0.508-0.508-0.633-1.254-0.376-1.88l-2.332-2.332v6.136c0.164 0.082 0.32 0.19 0.457 0.327 0.675 0.675 0.675 1.769 0 2.445-0.675 0.675-1.77 0.675-2.444 0-0.675-0.676-0.675-1.77 0-2.445 0.167-0.167 0.36-0.293 0.566-0.377v-6.193c-0.206-0.084-0.399-0.209-0.566-0.377-0.511-0.511-0.634-1.262-0.372-1.889l-2.557-2.558-6.753 6.752c-0.567 0.568-0.567 1.488 0 2.055l9.849 9.848c0.567 0.567 1.486 0.567 2.054 0l9.802-9.802c0.567-0.567 0.567-1.488 0-2.055z"
+                            ></path>
+                        </svg>
+                    </div>
+                </div>
+                <div className={`row ${styles.aboutTool}`}>
+                    <div className={styles.aboutHeading}>
+                        <span>Tools</span> I use
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            role="img"
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title></title>
+                            <path
+                                fill="currentColor"
+                                d="M23.15 2.587L18.21.21a1.494 1.494 0 0 0-1.705.29l-9.46 8.63-4.12-3.128a.999.999 0 0 0-1.276.057L.327 7.261A1 1 0 0 0 .326 8.74L3.899 12 .326 15.26a1 1 0 0 0 .001 1.479L1.65 17.94a.999.999 0 0 0 1.276.057l4.12-3.128 9.46 8.63a1.492 1.492 0 0 0 1.704.29l4.942-2.377A1.5 1.5 0 0 0 24 20.06V3.939a1.5 1.5 0 0 0-.85-1.352zm-5.146 14.861L10.826 12l7.178-5.448v10.896z"
+                            ></path>
+                        </svg>
+                    </div>
+                    <div className={`col-lg-2 ${styles.aboutIcon}`}>
+                        <svg
+                            role="img"
+                            viewBox="0 0 24 24"
+                            height="1em"
+                            width="1em"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <title></title>
+                            <path
+                                fill="currentColor"
+                                d="M13.527.099C6.955-.744.942 3.9.099 10.473c-.843 6.572 3.8 12.584 10.373 13.428 6.573.843 12.587-3.801 13.428-10.374C24.744 6.955 20.101.943 13.527.099zm2.471 7.485a.855.855 0 0 0-.593.25l-4.453 4.453-.307-.307-.643-.643c4.389-4.376 5.18-4.418 5.996-3.753zm-4.863 4.861l4.44-4.44a.62.62 0 1 1 .847.903l-4.699 4.125-.588-.588zm.33.694l-1.1.238a.06.06 0 0 1-.067-.032.06.06 0 0 1 .01-.073l.645-.645.512.512zm-2.803-.459l1.172-1.172.879.878-1.979.426a.074.074 0 0 1-.085-.039.072.072 0 0 1 .013-.093zm-3.646 6.058a.076.076 0 0 1-.069-.083.077.077 0 0 1 .022-.046h.002l.946-.946 1.222 1.222-2.123-.147zm2.425-1.256a.228.228 0 0 0-.117.256l.203.865a.125.125 0 0 1-.211.117h-.003l-.934-.934-.294-.295 3.762-3.758 1.82-.393.874.874c-1.255 1.102-2.971 2.201-5.1 3.268zm5.279-3.428h-.002l-.839-.839 4.699-4.125a.952.952 0 0 0 .119-.127c-.148 1.345-2.029 3.245-3.977 5.091zm3.657-6.46l-.003-.002a1.822 1.822 0 0 1 2.459-2.684l-1.61 1.613a.119.119 0 0 0 0 .169l1.247 1.247a1.817 1.817 0 0 1-2.093-.343zm2.578 0a1.714 1.714 0 0 1-.271.218h-.001l-1.207-1.207 1.533-1.533c.661.72.637 1.832-.054 2.522zM18.855 6.05a.143.143 0 0 0-.053.157.416.416 0 0 1-.053.45.14.14 0 0 0 .023.197.141.141 0 0 0 .084.03.14.14 0 0 0 .106-.05.691.691 0 0 0 .087-.751.138.138 0 0 0-.194-.033z"
+                            ></path>
+                        </svg>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default About
+export default About;
